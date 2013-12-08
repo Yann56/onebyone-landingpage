26e058dc45ccb39004c19c2ea072bd097a24855e
Andrea Benfatto <andreabenfatto@gmail.com>
2013-12-08 02:51:01 +0100

    indentation fix.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,21 +1,22 @@
 'use strict';
+/*jshint unused: false */
 
 angular.module('onebyoneApp')
   .controller('MainCtrl', function ($scope) {
-  })
+})
   .controller('ProjectCtrl', function ($scope) {
-  })
+})
   .controller('TeamCtrl', function ($scope) {
-  })
+})
   .directive('headerdirective', function() {
-	  	return {
-	  		restrict: 'E',
-	  		templateUrl: 'views/partial-header.html'
-	  	};
+    return {
+      restrict: 'E',
+      templateUrl: 'views/partial-header.html'
+    };
   })
   .directive('footerdirective', function() {
       return {
         restrict: 'E',
         templateUrl: 'views/partial-footer.html'
       };
-  });;
+    });
